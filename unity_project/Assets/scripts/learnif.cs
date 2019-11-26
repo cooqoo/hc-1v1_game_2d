e4a7b3c7a64ff18ccdc0238b78f96904aebafc0f
cooqoo <55131551+cooqoo@users.noreply.github.com>
2019-11-26 17:02:37 +0800

    動畫控制

---
+++ unity_project/Assets/scripts/learnif.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class learnif : MonoBehaviour
+{
+    public bool test;
+    public bool weapon;
+    //start 遊戲開始執行一次
+    void Start()
+    {
+        if (true)
+        {
+            print("測試");
+        }
+
+        if(test)
+        {
+            print("布林值勾選");
+        }
+
+        else
+        {
+            print("布林值取消勾選");
+        }
+        
+        if (weapon)
+        {
+            print("ak47");
+        }
+        
+        else
+        {
+            print("小刀");
+        }
+
+
+    }
+    //一秒執行60次
+    private void Update()
+    {
+        //Unity API
+        //輸入.取得按鍵(按鍵列舉.名稱)
+        print(Input.GetKeyDown(KeyCode.Space));
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            print("向右走");
+        }
+
+
+    }
+
+
+    
+
+
+
+}
--- unity_project/Assets/scripts/player.cs
+++ unity_project/Assets/scripts/player.cs
@@ -10,20 +10,98 @@ public class player : MonoBehaviour
     public bool isGround;
     public string playerName = "coo";
 
-    public AudioClip[] sounds; 
+    public AudioClip soundatk;
+    public AudioClip soundjump;
+    public AudioClip soundground;
+    public AudioClip soundhurt;
+    public AudioClip sounddie;
+
+    public Animator ani;
+
+    public AudioSource aud;
     
 
     //定義方法 method
+    
     public void move()
     {
-        print("移動");
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            print("向右走");
+            ani.SetBool("跑步開關", true);
+
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            print("向右走");
+            ani.SetBool("跑步開關", false);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            print("向左走");
+            ani.SetBool("跑步開關", true);
+
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            print("向左走");
+            ani.SetBool("跑步開關", false);
+        }
     }
+    public void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            print("跳躍");
+            ani.SetBool("跳躍", true);
+            aud.PlayOneShot(soundjump);
+        }
+    }
+    public void Hurt()
+    {
+        print("受傷");
+        ani.SetTrigger("hurt");
+        aud.PlayOneShot(soundhurt);
+    }
+    public void Dead()
+    {
+        print("死亡");
+        ani.SetBool("ko", true);
+        aud.PlayOneShot(sounddie,5);
+        
+    }
+    public void Attack()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            print("攻擊");
+            ani.SetTrigger("New Trigger");
+            aud.PlayOneShot(soundatk);
+        }
+    }
+    
     private void Start()
     {
-        move();
+       
     }
     private void Update()
     {
         move();
+        Attack();
+        Jump();
+
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        Dead();
+
+
+
+
+
+
     }
+
+
 }
